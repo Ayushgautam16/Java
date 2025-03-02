1dd86de46f83133032a9bb9bf09fd32c026290a8
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-02 10:30:36 +0530

    1

--- hello.java
+++ hello.java
@@ -1,5 +1,19 @@
-package Course-Java;
+// jshell> 2+3
+// $1 ==> 5
 
-public class hello {
-    
-}
+// jshell> 9-3
+// $2 ==> 6
+
+// jshell>
+
+
+// jshell> print(7)
+// |  Error:
+// |  cannot find symbol
+// |    symbol:   method print(int)
+// |  print(7)
+// |  ^---^
+
+// jshell> System.out.print("hello world")
+// hello world
+// jshell>
