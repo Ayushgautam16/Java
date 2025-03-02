d3c605fe110db1ac20ec4c2f28eb726385ccd084
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-02 10:34:10 +0530

    1

--- hello.java
+++ hello.java
@@ -17,3 +17,27 @@
 // jshell> System.out.print("hello world")
 // hello world
 // jshell>
+
+// JVM compatible 
+// os works on hardware
+// not accepting the code directly
+// java is platform independent
+// java is a statically typed language
+
+// Java supports Object Oriented Programming (OOP)
+
+// Java has a strong type system
+
+// Java has a garbage collector
+
+// Java has a multithreaded programming model
+
+// Java has a built-in exception handling mechanism
+
+// Java is a high-level language
+
+// Java is a compiled language
+
+// java has the main 
+// public static void main()
+
