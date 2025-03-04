4e7a129aeced55b7510fa19eb003ce239f8e3001
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-04 21:20:21 +0530

    1

--- array.java
+++ array.java
@@ -1,3 +1,13 @@
 public class array {
-    
+    public static void main(String[]args){
+        int nums[] = {1,2,3,4,5,6,7,8};
+        nums[4] =0;
+        System.out.println(nums[1]);
+        System.out.println(nums[4]);
+
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i] + " array hai ye");
+        }
+
+    }
 }
