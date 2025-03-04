7ef77690b086cd7a378911cc6b6963f6d60e0fd2
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-04 21:12:45 +0530

    1

--- operators.java
+++ operators.java
@@ -1,43 +1,43 @@
-public class operators {
-    public static void main(String[] args) {
+// public class operators {
+//     public static void main(String[] args) {
         
 
-// int x = 6;
-// int y = 5;
+// // int x = 6;
+// // int y = 5;
 
-// boolean result = x>y;
-// System.out.println(result);
+// // boolean result = x>y;
+// // System.out.println(result);
 
-// boolean result =  x!=y;
-// System.out.println(result);
+// // boolean result =  x!=y;
+// // System.out.println(result);
 
-// logical operator
+// // logical operator
 
-// int x = 6;
-// boolean result = x>5 && x<10;
-// System.out.println(result);
+// // int x = 6;
+// // boolean result = x>5 && x<10;
+// // System.out.println(result);
 
-// ternary operator
+// // ternary operator
 
-// int a = 10;
-// int b = 20;
-// int max = (a > b)? a : b;
-// System.out.println(max);
+// // int a = 10;
+// // int b = 20;
+// // int max = (a > b)? a : b;
+// // System.out.println(max);
 
-// && and 
-// || or 
-// ! not 
-
-
-int y = 6;
-boolean wht = x>5 || x<10;
-System.out.println(wht);
-
-int z = 6;
-boolean r = x>5 ;
-System.out.println(!r);
+// // && and 
+// // || or 
+// // ! not 
 
 
+// int y = 6;
+// boolean wht = x>5 || x<10;
+// System.out.println(wht);
 
-    }
-}
+// int z = 6;
+// boolean r = x>5 ;
+// System.out.println(!r);
+
+
+
+//     }
+// }
