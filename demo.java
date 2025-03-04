ee92b60edada3cd9b7fa219541cc75232167e57b
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-04 20:58:56 +0530

    1

---
+++ demo.java
@@ -0,0 +1,17 @@
+// class calculator {
+//     public int add(int n1, int n2){
+//          return  n1 + n2;
+//     }
+// }
+
+// public class demo{
+//     public static void main(String[] args){
+//         calculator cal = new calculator();
+//         int sum = cal.add(5, 10);
+//         System.out.println("Sum: " + sum);
+//     }
+// }
+
+
+// ********method overloading is the process in which we have diffrent parameter but same 
+
