22860395abd0e4f2ee0c76598e50b00f19155d7e
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-05 14:18:07 +0530

    1

---
+++ staticnum.java
@@ -0,0 +1,40 @@
+// class mobile {
+// String brand;
+// int price;
+// String network;
+// String name;
+
+//     public void show(){
+//         System.out.println(brand + " " + price + " " + network);
+
+//     }
+// }
+
+
+
+// public class staticnum {
+//     public static void main(String[] args) {
+//         mobile obj1 = new mobile();
+//         obj1.brand ="apple";
+//         obj1.price = 10000;
+//         obj1.network = "4G";
+//         obj1.name = "iPhone 12 Pro";
+
+// mobile obj2 = new mobile();
+//         obj2.brand ="samsung Android";
+//         obj2.price = 100000;
+//         obj2.network = "5G";
+//         obj2.name = "smarthphone";
+
+
+        
+
+
+//         obj1.show();
+//         obj2.show();
+//     }
+// }
+
+
+
+// static means you creating a new class member not the object member 
