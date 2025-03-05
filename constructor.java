7544f340a961b0f092ac5c3d8cdf4c10e3e2c230
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-05 15:28:53 +0530

    1

---
+++ constructor.java
@@ -0,0 +1,84 @@
+
+// class human
+// { 
+//     // private int age = 11;
+//     // private String name = "Ayush";
+
+//     private int age ;
+//     private String name ;
+
+
+//     // constructor with parameters
+
+// // define constructor() 
+// // constructor name should be class name
+// public human()
+//  {
+// // System.out.println("its constructor is " + name);
+
+// // default constructor
+
+
+// age = 23;               
+// name = "Age";
+
+// // by default we assign this value int he code 
+
+// // no need to call the constructor 
+
+// }
+
+// // parametrized constructor 
+
+// public human(int a, String n)
+//  {
+//     age = a;
+//     name = n;
+//  }
+
+
+    
+
+//    public int getage() { 
+//     return age;
+
+//    }
+//    public void setage(int a){
+//     age = a;
+//    }
+
+//    public String getname() { 
+//     return name;
+
+//    }
+//    public void setname(String n){
+//     name = n;
+//    }
+// }
+    
+
+// public class constructor{
+//     public static void main(String[] args){
+
+// // define the object of human here 
+// human hume = new human();
+// // hume.age =56;
+// // hume.name ="ayush";
+
+
+// // hume.setname("Ayush");
+// // hume.setage(20);
+
+// // System.out.println("Age of the person is :"+hume.age);
+
+// // System.out.println(hume.getname() + " :" + hume.getage());
+
+// System.out.println(hume.getname() + " :" + hume.getage());
+
+//     }
+// }
+
+
+
+
+class a
