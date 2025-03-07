4226d17c3b62a8ca1436c092e0f88f3d5e14ccb1
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-07 21:20:47 +0530

    1

--- polymorphism.java
+++ polymorphism.java
@@ -1,27 +1,68 @@
-class A{
-    void show(){
-        System.out.println("class A display");
-    }
-}
-class B extends A{
-    void show(){
-        System.out.println("class B display");
-    }
-}
+// class Computer{
+
+// }
+// class Laptop extends Computer{
+//     void show(){
+//         System.out.println("class Laptop display");
+//     }
+// }
 
 
-public class polymorphism {
-    public static void main(String[] args) {
+// class A{
+//     void show(){
+//         System.out.println("class A display");
+//     }
+// }
+// class B extends A{
+//     void show(){
+//         System.out.println("class B display");
+//     }
+// }
+
+
+// public class polymorphism {
+//     public static void main(String[] args) {
         
-        // polymorphism that has many behaviour 
-        // types of polymorphism are run time compile time 
-        // compile time polymorphism: static method overloading
-        // run time polymorphism: dynamic method overriding
+//         // polymorphism that has many behaviour 
+//         // types of polymorphism are run time compile time 
+//         // compile time polymorphism: static method overloading
+//         // run time polymorphism: dynamic method overriding
 
-        
+//         A obj = new B();
+//         obj.show();
+
+//         Computer comp = new Laptop();
+//        obj.show(); // dynamic method overriding
 
 
 
 
+//     }
+// }
+
+
+
+
+
+
+// final - var, method, class 
+
+class Calc{
+    final int x = 10;
+    final void show(){
+        System.out.println("class Calc show");
     }
+    public void add(int a, int b){
+System.out.println(a+b);
+    }
+public class polymorphism{
+    
+public static void main(String[] args) {Calc obj = new Calc(); 
+
+    obj.show(); // dynamic method overriding
+    obj.add(3, 8);
+    
+
 }
+}}
+
