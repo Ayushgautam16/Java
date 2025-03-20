a2085859e461041418e86e732a28ec6bae37ff19
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-20 22:32:46 +0530

    1

--- array.java
+++ array.java
@@ -129,7 +129,7 @@
 // import java.util.ArrayList;
 // import java.util.Arrays;
 // class array{
-    public static void main(String[]args){
+    // public static void main(String[]args){
 
 
        int arr[][] = new int [3][];
