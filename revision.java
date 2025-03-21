62b12204a3266d0e692b51901771b5d494a01614
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-21 17:52:18 +0530

    1

---
+++ revision.java
@@ -0,0 +1,44 @@
+public class revision {
+    public static void main(String[] args){
+        
+// long ts = 134;
+// long tsInt =(int)ts; 
+// //  here tsInt isliye likha hai taaki error na aaye var ka hi naam hai 
+// System.out.println(tsInt);
+
+// int age = 150;
+
+// // type casting type conversion
+
+// byte newage = (byte)age;
+// System.out.println(newage);
+// // -106
+         
+//     }
+
+//     public static void str (String[] args){
+//         System.out.println("Hello World");
+//     }
+
+    // boolean = true and false
+    // byte = 1 byte stores in btw -128 to 127
+    // short = 2 byte stores in btw -32768 to 32767
+    // int = 4 byte stores in btw -2147483648 to 2147483647
+    // long = 8 byte stores in btw -9223372036854775808 to 9223372036854775807
+    // float = 4 byte (1.2f) stores in btw 1.4e-045 to 3.4e+038 6 to 7 decimal digits
+    // double = 8 byte (1.2d) stores in btw 4.9e-324 to 1.8e+308 15 decimal digits
+    // char = 2 byte stores in btw 0 to 65535 single char 
+    // String = stores in btw 0 to 65535
+
+  
+// binary num system java operator
+
+
+
+
+
+    }
+
+
+}
+
