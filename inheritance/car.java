dc1072e6d308cbca2e68ba347c8bfd11c819317c
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-23 17:50:48 +0530

    1

---
+++ inheritance/car.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+ 
+// extends krke  hum dusri class ki value inherit kr skte hai or properties bhi 
+public class car extends vehicle{
+    public static void main(String[] args) {
+        car obj = new car();
+        obj.start();
+        obj.stop();
+    }
+   
+}
---
+++ inheritance/vehicle.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+// Parent class or superclass
+public class vehicle {
+    int wheels;
+    int headLights;
+    String color;
+
+void start(){
+System.out.println("vehicle is starting");
+}
+
+void stop(){
+    System.out.println("vehicle is stopping");
+    }
+
+
+    
+
+}
