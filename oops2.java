9291c5d037abb337e940745e0fa63d0c7adf40c8
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-23 17:18:56 +0530

    1

--- oops2.java
+++ oops2.java
@@ -1,3 +1,81 @@
-class oops2{
+// constructor 
+// 1. constructor is a special method that is used to initialize the object.
+// 2. constructor has the same name as the class name.
+// 3. constructor does not have a return type.
+// 4. constructor is called when the object is created.
+// 5. constructor is called only once for an object.
+// 6. constructor is used to initialize the object.
+// 7. constructor is used to allocate memory for the object.    
+// 8. constructor is used to initialize the instance variables of the object.
+// 9. constructor is used to perform any other initialization tasks.
 
-}
+// rule
+// 1. constructor name must be the same as the class name.
+// 2. constructor must have no explicit return type.
+// 3. constructor can not be static, abstract, final, and synchronized.
+// 4. constructor can not be inherited.
+// 5. constructor can not be overridden.
+// 6. constructor can be overloaded.
+// 7. constructor can have access specifiers.
+// 8. constructor can have parameters.
+// 9. constructor can not have a return statement.  
+// 10. constructor can not have a return type.
+
+
+// public class oops2{
+//     public static void main(String[] args) {
+//         constructor num1 = new constructor(2,3);
+//         constructor num2 = new constructor(4,5);
+//         num1.print();
+//         num2.print();
+//     }
+// }
+// class constructor{
+//     int a, b;
+
+//     public constructor(int a){
+//         a = real;
+//         System.out.println("constructor is called");
+//     }
+//     public constructor(){
+//         a = 10;
+//         b = 20;
+//         System.out.println("constructor is called");
+
+//         // example of the default constructor 
+
+
+//         // System.out.println("a: " + a);
+//         // System.out.println("b: " + b);
+//     }
+//     public constructor(int real, int imaginary){
+//        a = real;
+//        b = imaginary;
+//     }
+
+// void print(){
+//     System.out.println("a: " + a);
+//     System.out.println("b: " + b);
+
+// }
+
+// }
+// constructor add(constructor b){
+//     constructor temp = new constructor(a + num2.a, b+ num2.b);
+//     return temp;
+// }
+
+// // this keyword 
+// // 1. this keyword is a reference variable in Java that refers to the current object.
+// // 2. this keyword is used to refer to the current object in the class.
+
+// // used to remove the ambiguity in cariable names 
+
+// // we can invoke methods of the current class or to invoke a constructor of the cureent class 
+// // 3. this keyword is used to invoke the current class constructor
+// // 4. this keyword is used to pass the current object as a parameter to another method.
+
+// // this ke andar current obj ka refrence store hota hai 
+
+
+// inheritance //
