00638a5ad2ce70bb9b586138240fd0115689e5a5
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-25 20:44:07 +0530

    1

--- XOR.java
+++ XOR.java
@@ -1,15 +1,15 @@
-import java.util.*;
-class XOR{
-    static int findUnique(int[] arr) {
-        int result = 0;
-        for (int num : arr) {
-            result ^= num;  // XOR operation to find the unique element
-        }
-        return result;
-    }
+// import java.util.*;
+// class XOR{
+//     static int findUnique(int[] arr) {
+//         int result = 0;
+//         for (int num : arr) {
+//             result ^= num;  // XOR operation to find the unique element
+//         }
+//         return result;
+//     }
 
-    public static void main(String[] args) {
-        int[] arr = {2, 3, 5, 4, 5, 3, 4};
-        System.out.println(findUnique(arr));
-    }
-}
+//     public static void main(String[] args) {
+//         int[] arr = {2, 3, 5, 4, 5, 3, 4};
+//         System.out.println(findUnique(arr));
+//     }
+// }
