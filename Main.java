2a3d1d9e4d1f51b40e48d23b661192ba3c519389
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-27 22:03:46 +0530

    1

--- Main.java
+++ Main.java
@@ -20,7 +20,7 @@
    
 
 // public class Main {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
         System.out.println("Enter the value you wanted to enter: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
