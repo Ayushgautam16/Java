0f7c183c12fb8ef36622e87c23923291789bcfca
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-03-29 23:45:11 +0530

    1

--- hackerrank.java
+++ hackerrank.java
@@ -1,1 +1,16 @@
-cla
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        
+        while (scanner.hasNextLine()) {  // Continue reading until EOF
+            count++;
+            String line = scanner.nextLine();
+            System.out.println(count + " " + line);
+        }
+        
+        scanner.close();  // Close scanner to prevent resource leaks
+    }
+}
