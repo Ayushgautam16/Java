db2476b07d1e0e674252fe99a76cfc67d78cfa05
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-09 17:56:13 +0530

    1

--- Swap.java
+++ Swap.java
@@ -116,8 +116,8 @@
     // public static void main(String[] args){
         // int a = 10;
         // int b = 20;
-        System.out.println("a: " + a + ", b: " + b);
+        // System.out.println("a: " + a + ", b: " + b);
 
-    }
+    // }
 
 }
