2d4fb04dcdd6dbd1a7ebbf286a167687bd65258f
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-15 12:18:48 +0530

    1

--- graph.java
+++ graph.java
@@ -29,7 +29,29 @@ public class graph {
         // adjacency list representation
 
 
+// graph representation in matrix form {{0,1},{1,2},{2,3},{3,4}}
 
+// List<List<Integer>> graph = new ArrayList<>();
+//         for(int i = 0; i < 4; i++){
+//             graph.add(new ArrayList<>());
+//         }   
+//         graph.get(0).add(0);
+//         graph.get(0).add(1);
+//         graph.get(1).add(1);
+//         graph.get(1).add(2);
+//         graph.get(2).add(2);
+//         graph.get(2).add(3);
+//         graph.get(3).add(3);
+//         graph.get(3).add(4);
+//         System.out.println(graph);
+
+
+// adjacency list 
+List<integer> graph = new ArrayList<>();
+
+// add vertices
+
+graph.add(new ArrayList<>());
 
 
     }
