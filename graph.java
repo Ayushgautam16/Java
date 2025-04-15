4b9db4b23a6792d2b18fdb5f3b1f1a885f047b32
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-15 11:56:27 +0530

    1

---
+++ graph.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class graph {
+    public static void main(String[] args) {
+        
+        // types of graph directed and undirected graph 
+        // directed graph example
+// path and walk
+
+// input in the graph 
+        // adjacency matrix representation
+    //    int [][] graph =  {{0,1},{0,2},{1,3}}
+    //    System.out.println(graph)
+
+// representation of the graph by the matrix using input in undirected graph
+
+    //    List<List<Integer>> graph = new ArrayList<>();
+    //    for(int i = 0; i < 4; i++){
+    //        graph.add(new ArrayList<>());
+    //    }
+    //    graph.get(0).add(1);
+    //    graph.get(0).add(2);
+    //    graph.get(1).add(3);
+    //    graph.get(2).add(0);
+    //    graph.get(3).add(1);
+    //    System.out.println(graph);
+
+
+        // traversal of graph
+        // adjacency list representation
+
+
+
+
+
+    }
+}
