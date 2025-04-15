7c6033a9b0d6ddb56d164e3e979b1f1fef14f83e
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-15 22:24:03 +0530

    1

---
+++ Pepcoding.java/basic.java
@@ -0,0 +1,41 @@
+// import java.util.*;
+
+// public class basic{
+//     public static void main(String []args){
+// System.out.println("hello world");
+//     }
+
+// }
+
+
+// import java.util.*;
+
+// public class basic{
+//     public static void main(String []args){
+// System.out.println("*\n**\n***\n****\n");
+
+// *
+// **
+// ***
+// ****
+
+
+// System.out.println("*****");
+// System.out.println("   * ");
+// System.out.println("  *  ");
+// System.out.println(" *   ");
+// System.out.println("*****");
+
+
+// *****
+//    *
+//   *
+//  *
+// *****
+
+//     }
+
+// }
+
+// ***********Data type***********
+
