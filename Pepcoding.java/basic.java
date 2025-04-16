995fa301402f8740c3ddb50a39d5951209f242c0
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-16 18:43:49 +0530

    1

--- Pepcoding.java/basic.java
+++ Pepcoding.java/basic.java
@@ -47,17 +47,3 @@
 // float	4 bytes	32-bit floating point
 
 
-import java.util.*;
-
-public class basic{
-    public static void main(String []args){
-Scanner scn = new Scanner(System.in);
-int t =  scn.nextInt();
-for (int i = 0; i<t; i++){
-    int n = scn.nextINt();
-
-
-}
-    }
-
-}
---
+++ Pepcoding.java/isprime.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class isprime{
+    public static void main(String []args){
+Scanner scn = new Scanner(System.in);
+// int t =  scn.nextInt();
+// for (int i = 0; i<t; i++){
+//     int n = scn.nextInt();
+//     int count=0;
+//     for(int div = 1 ; div<=n; div++){
+//         if (n%div ==0){
+//             count++;
+//         }
+//     }
+//     if(count ==2){
+//         System.out.println("num is prime ");
+//     }
+//     else
+//     {
+//         System.out.println("not prime");
+//     }
+
+// }
+
+
+
+
+// but with this approach time limit exceeded
+
+
+    }
+
+}
