4839820ae2fd23a39d25e2786a02ab0b24d59f8c
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2025-04-16 18:21:39 +0530

    1

--- Pepcoding.java/basic.java
+++ Pepcoding.java/basic.java
@@ -39,3 +39,11 @@
 
 // ***********Data type***********
 
+// int	4 bytes	Standard 32-bit integer
+// char	2 bytes	Because Java uses UTF-16 encoding
+// boolean	1 byte (or JVM-dependent)	Technically 1 bit, but often rounded to 1 byte or more in JVM
+// double	8 bytes	64-bit floating point
+// long	8 bytes	64-bit integer
+// float	4 bytes	32-bit floating point
+
+    
